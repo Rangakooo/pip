2a377833110552b7307bac5cedfc75333a2004b8
Leon Skinner <Leon.skinner@bt.com>
2024-04-18 10:45:16 +0100

    init

--- Pipboy/pipboy.py
+++ Pipboy/pipboy.py
@@ -6,25 +6,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/stats.html')
+@app.route('/stats')
 def stats():
     return render_template('stats.html')
 
-@app.route('/inv.html')
+@app.route('/inv')
 def inv():
     return render_template('inv.html')
 
-@app.route('/data.html')
+@app.route('/data')
 def data():
     return render_template('data.html')
 
-@app.route('/map.html')
+@app.route('/map')
 def map():
     return render_template('map.html')
 
-@app.route('/radio.html')
+@app.route('/radio')
 def radio():
-    return render_template('radio.html')
+    return render_template('radio')
+
+@app.route('/test')
+def test():
+    return render_template('main')
 
 if __name__ == "__main__":
     app.run(debug=True)
