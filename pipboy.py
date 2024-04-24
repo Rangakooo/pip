9ea8023e64d584f7f3044a3f3b7888953d8630b0
Hal <45074457+Rangakooo@users.noreply.github.com>
2024-04-24 15:51:36 +0100

    Added inventory capabilities

--- pipboy.py
+++ pipboy.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-print("hello")
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///inventory.sqlite3'
+
+db = SQLAlchemy(app)
+
+class inventory(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    quantity = db.Column(db.Integer)
+    category = db.Column(db.String(100))
+
+    def __init__(self, name, quantity, category):
+        self.name = name
+        self.quantity = quantity
+        self.category = category
 
 @app.route('/')
 def index():
@@ -13,7 +27,8 @@ def stats():
 
 @app.route('/inv.html')
 def inv():
-    return render_template('inv.html')
+    items = inventory.query.all()
+    return render_template('inv.html', items=items)
 
 @app.route('/data.html')
 def data():
@@ -27,5 +42,22 @@ def map():
 def radio():
     return render_template('radio.html')
 
+@app.route('/special.html')
+def special():
+    return render_template('special.html')
+
+
+@app.route('/inv/show_all_items')
+def show_all_items():
+    items = inventory.query.all()
+    return render_template('inv.html', items=items)
+
+@app.route('/inv/show_items_category/<category>')
+def show_items_category(category):
+    items = inventory.query.filter_by(category=category).all()
+    return render_template('inv.html', items=items)
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
